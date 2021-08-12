508871335864b71a07e0798319e4f065c1134bd1
samuelmarcos <samuelmarcos9580@gmail.com>
2021-08-11 21:53:27 -0300

    add image analysis

--- demo02-sls-framework/run.sh
+++ demo02-sls-framework/run.sh
@@ -1,1 +1,27 @@
+#instalar
+
 npm i -g serverless
+
+#iniciar 
+
+serverless || sls 
+
+#sempre fazer deploy antes de tudo, para verificar se esta tudo ok 
+
+sls deploy
+
+#invocar na AWS
+
+sls invoke -f nome_handle
+
+#invocar
+
+sls invoke local -f nome_handle
+
+#configurar dashboard
+
+sls
+
+#logs
+
+sls logs -f nome_handle --tail
